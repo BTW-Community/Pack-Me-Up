955c64a55ea1ad114e705d62b7d8cd0281107072
DraViGen <fireshadoxe@gmail.com>
2025-12-20 16:53:37 +0100

    remove trash

--- Let's Automate/src/main/java/net/dravigen/lets_automate/LetsAutomateMod.java
+++
@@ -1,9 +0,0 @@
-package net.dravigen.lets_automate;
-
-import net.fabricmc.api.ModInitializer;
-
-public class LetsAutomateMod implements ModInitializer {
-	@Override
-	public void onInitialize() {
-	}
-}
